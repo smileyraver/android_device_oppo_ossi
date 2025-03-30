ece72b5f0d24cd8ac06da2b9642161188c2c4147
smileyraver <39043246+smileyraver@users.noreply.github.com>
2025-03-31 04:22:42 +1000

    Update and rename omni_ossi.mk to twrp_ossi.mk

--- twrp_ossi.mk
+++ twrp_ossi.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from ossi device
 $(call inherit-product, device/oplus/ossi/device.mk)
 
 PRODUCT_DEVICE := ossi
-PRODUCT_NAME := omni_ossi
+PRODUCT_NAME := twrp_ossi
 PRODUCT_BRAND := oplus
 PRODUCT_MODEL := ossi
 PRODUCT_MANUFACTURER := oplus
